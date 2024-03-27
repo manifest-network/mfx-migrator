e1bb9276771e1cf41faa5d8b305aa128020217ca
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-03-27 13:35:38 -0400

    fix: claim cmd

--- scripts/claim_and_migrate.sh
+++ scripts/claim_and_migrate.sh
@@ -4,8 +4,8 @@ WORKDIR=/jobs
 
 cd "$WORKDIR" || exit 1
 
-# Claim some work
-$APP claim
+# Claim some work from queue
+mfx-migrator claim
 
-# Run the migration
+# Run the migration for each JSON file in the workdir
 find . -name '*.json' ! -name "config.json" -print0 | xargs -0 -I{} -P 1 bash -c 'mfx-migrator migrate --uuid $(basename "{}" .json)'
