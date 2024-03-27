395ef6a60b57957473f26434b3f1e6483c5faa6d
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-03-27 13:31:58 -0400

    fix: migrate cmd, maybe

--- scripts/claim_and_migrate.sh
+++ scripts/claim_and_migrate.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-APP=mfx-migrator
 WORKDIR=/jobs
 
 cd "$WORKDIR" || exit 1
@@ -9,4 +8,4 @@ cd "$WORKDIR" || exit 1
 $APP claim
 
 # Run the migration
-find . -name '*.json' ! -name "config.json" -print0 | xargs -0 -I{} -P 1 bash -c '$APP migrate --uuid $(basename "{}" .json)'
+find . -name '*.json' ! -name "config.json" -print0 | xargs -0 -I{} -P 1 bash -c 'mfx-migrator migrate --uuid $(basename "{}" .json)'
