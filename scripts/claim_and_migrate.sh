f688aed538e11d2a5dcfc80f623d692f4138136e
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-03-27 13:25:30 -0400

    fix: migrate cmd

--- scripts/claim_and_migrate.sh
+++ scripts/claim_and_migrate.sh
@@ -9,4 +9,4 @@ cd "$WORKDIR" || exit 1
 $APP claim
 
 # Run the migration
-find . -name '*.json' ! -name "config.json" -print0 | xargs -0 -I{} -P 1 bash -c "$APP migrate --uuid $(basename {} .json)"
+find . -name '*.json' ! -name "config.json" -print0 | xargs -0 -I{} -P 1 bash -c '$APP migrate --uuid $(basename "{}" .json)'
