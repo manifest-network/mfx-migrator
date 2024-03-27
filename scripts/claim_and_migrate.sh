41bca00208355b8f8e4a977a410136e6406c8190
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-03-27 10:41:43 -0400

    feat: docker migration service

---
+++ scripts/claim_and_migrate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+APP=mfx-migrator
+WORKDIR=/jobs
+
+cd "$WORKDIR" || exit 1
+
+# Claim some work
+$APP claim
+
+# Run the migration
+find . -name '*.json' ! -name "config.json" -print0 | xargs -0 -I{} -P 1 bash -c "$APP migrate --uuid $(basename \"{}\" .json)"
