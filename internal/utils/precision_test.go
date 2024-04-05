c7b1262f54b3347e1f4db993b8a85a843c4fb22e
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-04-05 10:40:55 -0400

    fix: precision conversion
    1 MFX == 1000 umfx, not the other way around

--- internal/utils/precision_test.go
+++ internal/utils/precision_test.go
@@ -10,14 +10,16 @@ import (
 )
 
 func TestConvertPrecision(t *testing.T) {
-	n1 := "1"                          // 1e1
-	n3 := "1000"                       // 1e3
-	n24 := "1000000000000000000000000" // 1e24
-	n18 := "1000000000000000000"       // 1e18
+	n1 := "1"                                // 1e1
+	n3 := "1000"                             // 1e3
+	n24 := "1000000000000000000000000"       // 1e24
+	n30 := "1000000000000000000000000000000" // 1e30
+	n18 := "1000000000000000000"             // 1e18
+	n12 := "1000000000000"                   // 1e12
 	nF := "12.345"
 
-	bigIntValue24, _ := new(big.Int).SetString(n24, 10)
-	bigIntValue18, _ := new(big.Int).SetString(n18, 10)
+	bigIntValue30, _ := new(big.Int).SetString(n30, 10)
+	bigIntValue12, _ := new(big.Int).SetString(n12, 10)
 
 	tt := []struct {
 		name             string
@@ -28,10 +30,11 @@ func TestConvertPrecision(t *testing.T) {
 		err              string
 	}{
 		{name: "no precision change", n: n18, currentPrecision: 18, targetPrecision: 18, expected: nil, err: "current precision is equal to target precision: 18"},
-		{name: "increase precision", n: n18, currentPrecision: 18, targetPrecision: 24, expected: bigIntValue24},
-		{name: "decrease precision", n: n24, currentPrecision: 24, targetPrecision: 18, expected: bigIntValue18},
-		{name: "decrease precision 2", n: n3, currentPrecision: 9, targetPrecision: 6, expected: big.NewInt(1)},
-		{name: "invalid conversion (amount <= 0)", n: n1, currentPrecision: 3, targetPrecision: 1, err: "amount after conversion is less than or equal to 0: 0"},
+		{name: "increase precision", n: n18, currentPrecision: 18, targetPrecision: 24, expected: bigIntValue12},
+		{name: "decrease precision", n: n24, currentPrecision: 24, targetPrecision: 18, expected: bigIntValue30},
+		{name: "decrease precision 2", n: n3, currentPrecision: 9, targetPrecision: 6, expected: big.NewInt(1000000)},
+		{name: "decrease precision 3", n: n1, currentPrecision: 9, targetPrecision: 6, expected: big.NewInt(1000)},
+		{name: "invalid conversion (amount <= 0)", n: n1, currentPrecision: 1, targetPrecision: 3, err: "amount after conversion is less than or equal to 0: 0"},
 		{name: "invalid number (scientific notation)", n: "1e18", currentPrecision: 18, targetPrecision: 24, err: "error parsing big.Int: 1e18"},
 		{name: "invalid number (not a number)", n: "foo", currentPrecision: 18, targetPrecision: 24, err: "error parsing big.Int: foo"},
 		{name: "invalid number (empty string)", n: "", currentPrecision: 18, targetPrecision: 24, err: "error parsing big.Int: "},
